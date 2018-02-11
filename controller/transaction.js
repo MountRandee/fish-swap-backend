991477187f61078c83777ca044b2f3be7dbabf4a
Randy Cheung <randycheung.cy@gmail.com>
2018-02-11 03:39:00 -0500

    transaction post finished

--- controller/transaction.js
+++ controller/transaction.js
@@ -1,13 +1,30 @@
 var 
   express = require('express'),
   router = express.Router(),
+  bodyParser = require('body-parser'),
   Transaction = require('../models/transaction');
+
+// support json/url encoded bodies
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/", function(req, res) {
   Transaction.find(function(err, transactions) {
     console.log(transactions);
     if (err) res.send(err);
     res.send(transactions)
+  });
+});
+
+
+// { "date" : "2018-02-11", "sellerId" : 1, "buyerId" : 1, "fishId" : 1, "countryId" : 2, "price" : 1.05, "quantity" : 10 }
+router.post("/", function(req, res) {
+  var transaction = new Transaction(req.body);
+  transaction.save(function(err, createdObject) {
+    if (err) {
+      res.status(500).send(false);
+    }
+    res.status(200).send(true);
   });
 });
 
--- models/transaction.js
+++ models/transaction.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 
 var TransactionSchema = mongoose.Schema({
   date : String,
-  sellerId : String, 
-  buyerId: String,
+  sellerId : Number, 
+  buyerId: Number,
   fishId: Number,
   countryId: Number,
   price: Number, 
   quantity: Number
+},{
+  versionKey: false 
 });
 
 module.exports = mongoose.model("Transaction", TransactionSchema, "transaction");
