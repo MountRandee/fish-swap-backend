9318ae2357eeef4589a06aba3610143128f16ab6
Randy Cheung <randycheung.cy@gmail.com>
2018-02-11 03:14:53 -0500

    login controller done

--- controller/login.js
+++ controller/login.js
@@ -11,13 +11,25 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.post("/", function(req, res) {
   var phoneNumber = req.body.phoneNumber;
   var password = req.body.password;
+  
   if (phoneNumber  == null || password == null) {
-    res.send(null);
+    res.send(false);
   }
-  Seller.findOne({"phoneNumber": phoneNumber, "password": password}, "phoneNumber", function (err, seller) {
-    if (err) res.send(err);
-    console.log(seller);
+
+  Seller.findOne().
+  where('phoneNumber').equals(phoneNumber).
+  where('password').equals(password).
+  exec(function(err, seller) {
+    if (err) console.log("err:" + err);
+    
+    // res.send did not complete function without else condition !?!
+    if (seller) {
+      res.send(true);
+    } else {
+      res.send(false);
+    }
   });
+
 });
 
 module.exports = router;
